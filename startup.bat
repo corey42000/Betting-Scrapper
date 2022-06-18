78c7a3419f32e1ce3cf90a5f83471fd61ff795a3
NuttyC <uwnext@protonmail.com>
2022-06-18 12:52:14 -0700

    Adding Dependency script and programs to install.

---
+++ startup.bat
@@ -0,0 +1,15 @@
+REM This is a simple Batch file that installs the programs and tools to scrape deep into a website
+
+
+echo installing Dependancies
+echo -------------
+echo installing Python
+C:\Users\Administrator\Documents\projects\programs\python-3.10.5-amd64.exe
+echo Updating Chrome
+C:\Users\Administrator\Documents\projects\programs\ChromeSetup.exe 
+
+
+REM need to figre out a selenium install solution.
+REM Need to move into PS Script so the chrome driver is not started until scrapping is ready.
+REM echo installing chrome driver
+REM C:\Users\Administrator\Documents\projects\programs\chromedriver.exe
